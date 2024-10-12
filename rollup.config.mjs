07d78619ea0197550106b0607801684e667be09a
SSE World <suryakantmaurya2007@gmail.com>
2024-10-12 17:59:27 +0530

    Commit 9
    Created new Buttons

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -42,4 +42,77 @@ const config = {
   ],
 };
 
-export default [config];
+const button = {
+  input: "buttons/index.ts",
+  output: [
+    {
+      dir: "dist",
+      entryFileNames: "buttons.es.js",
+      format: "es",
+      sourcemap: true,
+    },
+    {
+      dir: "dist",
+      entryFileNames: "buttons.cjs.js",
+      format: "cjs",
+      sourcemap: true,
+    },
+  ],
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+    "react/jsx-runtime",
+    "react/jsx-dev-runtime",
+    // "crypto",
+    // "node:crypto"
+  ],
+  plugins: [
+    json(),
+    typescript({
+      tsconfig:
+        process.env.NODE_ENV === "production"
+          ? "./tsconfig.production.json"
+          : "./tsconfig.json",
+    }),
+  ],
+};
+
+export default [config, button];
+
+// /**
+//  * @type {import('rollup').RollupOptions}
+//  */
+// const createConfig = (input) => ({
+//   input: input,
+//   output: [
+//     {
+//       dir: "dist",
+//       entryFileNames: `${input.split("/").pop().split(".")[0]}.es.js`,
+//       format: "es",
+//       sourcemap: true,
+//     },
+//     {
+//       dir: "dist",
+//       entryFileNames: `${input.split("/").pop().split(".")[0]}.cjs.js`,
+//       format: "cjs",
+//       sourcemap: true,
+//     },
+//   ],
+//   external: [
+//     ...Object.keys(pkg.dependencies || {}),
+//     ...Object.keys(pkg.peerDependencies || {}),
+//     "react/jsx-runtime",
+//     "react/jsx-dev-runtime",
+//   ],
+//   plugins: [
+//     json(),
+//     typescript({
+//       tsconfig:
+//         process.env.NODE_ENV === "production"
+//           ? "./tsconfig.production.json"
+//           : "./tsconfig.json",
+//     }),
+//   ],
+// });
+
+// export default [createConfig("index.ts"), createConfig("buttons/index.ts")]
